34f040ca9995ba4bc07823819aa733896cbaa49e
Jonas Pardon <jonas@contractify.be>
2022-05-07 23:42:19 +0200

    Add initial config

--- config/event-visualizer.php
+++ config/event-visualizer.php
@@ -1,5 +1,31 @@
 <?php
-// config for JonasPardon/LaravelEventVisualizer
-return [
 
+return [
+    /**
+     * When enabled, this will include all default Laravel events
+     * like Login, Logout, JobFailed, ...
+     */
+    'show_laravel_events' => false,
+
+    /**
+     * When using subscribers, you can use different handler methods in the
+     * subscriber class. This will include the names of the handler methods
+     * in the visualization in the format 'LoginSubscriber-handleUserLogin'.
+     */
+    'show_subscriber_internal_handler_methods' => false,
+
+    /**
+     * All classes included here will be omitted from the visualization.
+     */
+    'classes_to_ignore' => [
+        // Illuminate\Auth\Events\Login::class,
+    ],
+
+    'theme' => [
+        'colors' => [
+            'event' => '#55efc4',
+            'listener' => '#74b9ff',
+            'job' => '#a29bfe',
+        ],
+    ],
 ];
